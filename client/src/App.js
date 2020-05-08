93cf89c82401968b3234dd010bec925f8741e891
skeshavaa <s.keshavaa@gmail.com>
2020-05-08 19:00:33 -0400

    started working on home

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'typeface-roboto';
+import './styles/general.css';
 
 import Home from './pages/home';
 import About from './pages/about';
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,39 +1,43 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import { Container, Box } from '@material-ui/core'
+import Keshavaa from '../assets/images/me.JPG'
+import styled from 'styled-components';
 
 const Home = () => {
 
-    const [Projects, setProjects] = useState([]);
-    const [Loading, setLoading] = useState(true)
-
-    
-
-    if (!Loading){
-        console.log(Projects);
-    } else{
-        axios.get('/api/items')
-        .then((projects) => {
-            setProjects(projects)
-            setLoading(false)
-        })
-    }
-    
-    if (Loading){
-        return(
-            <div>
-                <h1>Loading</h1>
-            </div>
-        )
-    } else{
-        return(
-            <div>
-                {Projects.data.map((project) => {
-                    return <h1>{project.name}</h1>
-                })}
-        </div>
-        )
-    }
+    return(
+        <Wrapper>
+            <CenterText>
+                <Text>Hey <br/> Check my stuff out</Text>
+            </CenterText>
+            <Box display="flex" flexDirection="column" justifyContent="center">
+            <Avatar src={Keshavaa} />
+            </Box>
+        </Wrapper>
+    )
 }
 
 export default Home
+
+const Wrapper = styled.div`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+`
+
+const CenterText = styled.div`
+    text-align: center;
+`
+
+const Text = styled.p`
+    font-family: 'Inconsolata';
+    font-size: 50px;
+`
+
+const Avatar = styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+`
 
