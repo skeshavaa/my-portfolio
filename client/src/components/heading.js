12e67cde7894af1cd4e79ec83839f59658b0d325
skeshavaa <s.keshavaa@gmail.com>
2020-05-10 13:48:10 -0400

    added heading and routing

---
+++ client/src/components/heading.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Heading = (props) => {
+    return (
+        <Wrapper>
+            <Title>{props.title}</Title>
+        </Wrapper>
+    )
+}
+
+export default Heading
+
+const Wrapper = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+`
+
+const Title = styled.h1`
+    font-size: 45px;
+    font-family: 'Abel';
+    
+`
--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layout'
+import Heading from '../components/heading'
+import axios from 'axios'
 
 const Projects = () => {
+
+    const [Data, setData] = useState([])
+    const [Loading, setLoading] = useState(true)
+
+    axios.get('api/items')
+        .then((projects) => {
+            setData(projects);
+            setLoading(false);
+        })
+    
+
+    let place = Loading ? <h1>true</h1> : <h1>false</h1>
+
     return (
         <div>
             <Layout />
+            <Heading title="Projects"/>
+            {place}
         </div>
     )
 }
