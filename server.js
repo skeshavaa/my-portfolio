03f740b007339ae097d84a07f9712e7516bf69c5
skeshavaa <s.keshavaa@gmail.com>
2020-05-13 14:54:53 -0400

    version 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -16,6 +17,14 @@ mongoose
     .catch(err => console.log(err));
 
     app.use('/api/items',projects);
+
+    if (process.env.NODE_ENV === 'production'){
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+    } 
 
     const port = process.env.PORT || 5000;
 
